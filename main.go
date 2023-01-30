d9692fc34ba5d8893d992d30683571b81829fdb7
Petri-Johan Last <petri.last@sourcegraph.com>
2023-01-30 08:58:44 +0200

    Add no-stream example

--- main.go
+++ main.go
@@ -70,9 +70,27 @@ func (p *ProfileTemplate) Render(w http.ResponseWriter) error {
 	return nil
 }
 
+type ProfileTemplateNoStream struct {
+	User bool
+}
+
+func (p *ProfileTemplateNoStream) Render(w http.ResponseWriter) error {
+	err := userTmpl.ExecuteTemplate(w, "profile", struct {
+		User bool
+	}{
+		User: p.User,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 type Product struct {
 	Name        string
 	Description string
+	Image       string
 }
 
 type ProductsTemplate struct {
@@ -84,6 +102,23 @@ func (p *ProductsTemplate) Render(w http.ResponseWriter) error {
 		Products []Product
 	}{
 		Products: <-p.Products,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type ProductsTemplateNoStream struct {
+	Products []Product
+}
+
+func (p *ProductsTemplateNoStream) Render(w http.ResponseWriter) error {
+	err := productsTmpl.ExecuteTemplate(w, "products", struct {
+		Products []Product
+	}{
+		Products: p.Products,
 	})
 	if err != nil {
 		return err
@@ -108,56 +143,87 @@ func (c *CategoriesTemplate) Render(w http.ResponseWriter) error {
 	return nil
 }
 
-func slowBoolFetch(t time.Duration, c chan bool) {
-	time.Sleep(t * time.Millisecond)
-	c <- true
+type CategoriesTemplateNoStream struct {
+	Categories []string
 }
 
-func slowFetchProducts(t time.Duration, c chan []Product) {
-	time.Sleep(t * time.Millisecond)
-	c <- []Product{
-		{
-			Name:        "Product 1",
-			Description: "Product 1 Description",
-		},
-		{
-			Name:        "Product 2",
-			Description: "Product 2 Description",
-		},
-		{
-			Name:        "Product 3",
-			Description: "Product 3 Description",
-		},
-		{
-			Name:        "Product 4",
-			Description: "Product 4 Description",
-		},
-		{
-			Name:        "Product 5",
-			Description: "Product 5 Description",
-		},
-		{
-			Name:        "Product 6",
-			Description: "Product 6 Description",
-		},
+func (c *CategoriesTemplateNoStream) Render(w http.ResponseWriter) error {
+	err := categoriesTmpl.ExecuteTemplate(w, "categories", struct {
+		Categories []string
+	}{
+		Categories: c.Categories,
+	})
+	if err != nil {
+		return err
 	}
+	return nil
 }
 
-func slowFetchCategories(t time.Duration, c chan []string) {
-	time.Sleep(t * time.Millisecond)
-	c <- []string{
-		"Category 1",
-		"Category 2",
-	}
+func slowBoolFetch(t time.Duration) chan bool {
+	c := make(chan bool)
+	go func() {
+		time.Sleep(t * time.Millisecond)
+		c <- true
+	}()
+
+	return c
+}
+
+func slowFetchProducts(t time.Duration) chan []Product {
+	c := make(chan []Product)
+	go func() {
+		time.Sleep(t * time.Millisecond)
+		c <- []Product{
+			{
+				Name:        "Product 1",
+				Description: "Product 1 Description",
+				Image:       "/static/camera.jpeg",
+			},
+			{
+				Name:        "Product 2",
+				Description: "Product 2 Description",
+				Image:       "/static/cbd.jpeg",
+			},
+			{
+				Name:        "Product 3",
+				Description: "Product 3 Description",
+				Image:       "/static/coke.jpeg",
+			},
+			{
+				Name:        "Product 4",
+				Description: "Product 4 Description",
+				Image:       "/static/cream.webp",
+			},
+			{
+				Name:        "Product 5",
+				Description: "Product 5 Description",
+				Image:       "/static/laptop.jpg",
+			},
+			{
+				Name:        "Product 6",
+				Description: "Product 6 Description",
+				Image:       "/static/phone.jpeg",
+			},
+		}
+	}()
+
+	return c
+}
+
+func slowFetchCategories(t time.Duration) chan []string {
+	c := make(chan []string)
+	go func() {
+		time.Sleep(t * time.Millisecond)
+		c <- []string{
+			"Category 1",
+			"Category 2",
+		}
+	}()
+
+	return c
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	c1 := make(chan bool)
-	c2 := make(chan []Product)
-	c3 := make(chan []string)
-	go slowBoolFetch(time.Duration(rand.Intn(1000)), c1)
-	go slowFetchProducts(time.Duration(rand.Intn(4000)), c2)
-	go slowFetchCategories(time.Duration(rand.Intn(3000)), c3)
 	flusher, _ := w.(http.Flusher)
 	err := baseTmpl.ExecuteTemplate(w, "base", struct {
 		Title      string
@@ -169,7 +235,7 @@ func index(w http.ResponseWriter, req *http.Request) {
 		Profile: &TemplateLoader{
 			ID: "profile",
 			Template: &ProfileTemplate{
-				User: c1,
+				User: slowBoolFetch(time.Duration(rand.Intn(1000))),
 			},
 			W:       w,
 			Flusher: flusher,
@@ -177,7 +243,7 @@ func index(w http.ResponseWriter, req *http.Request) {
 		Products: &TemplateLoader{
 			ID: "products",
 			Template: &ProductsTemplate{
-				Products: c2,
+				Products: slowFetchProducts(time.Duration(rand.Intn(4000))),
 			},
 			W:       w,
 			Flusher: flusher,
@@ -185,6 +251,48 @@ func index(w http.ResponseWriter, req *http.Request) {
 		Categories: &TemplateLoader{
 			ID: "categories",
 			Template: &CategoriesTemplate{
+				Categories: slowFetchCategories(time.Duration(rand.Intn(3000))),
+			},
+			W:       w,
+			Flusher: flusher,
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func indexNoStream(w http.ResponseWriter, req *http.Request) {
+	c1 := <-slowBoolFetch(time.Duration(rand.Intn(1000)))
+	c2 := <-slowFetchProducts(time.Duration(rand.Intn(4000)))
+	c3 := <-slowFetchCategories(time.Duration(rand.Intn(3000)))
+	flusher, _ := w.(http.Flusher)
+	err := baseTmpl.ExecuteTemplate(w, "base", struct {
+		Title      string
+		Profile    *TemplateLoader
+		Products   *TemplateLoader
+		Categories *TemplateLoader
+	}{
+		Title: "Hello World",
+		Profile: &TemplateLoader{
+			ID: "profile",
+			Template: &ProfileTemplateNoStream{
+				User: c1,
+			},
+			W:       w,
+			Flusher: flusher,
+		},
+		Products: &TemplateLoader{
+			ID: "products",
+			Template: &ProductsTemplateNoStream{
+				Products: c2,
+			},
+			W:       w,
+			Flusher: flusher,
+		},
+		Categories: &TemplateLoader{
+			ID: "categories",
+			Template: &CategoriesTemplateNoStream{
 				Categories: c3,
 			},
 			W:       w,
@@ -199,8 +307,11 @@ func index(w http.ResponseWriter, req *http.Request) {
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", index)
+	http.HandleFunc("/stream", index)
+	http.HandleFunc("/nostream", indexNoStream)
 
+	fmt.Println("Stream version available at http://localhost:1337/stream")
+	fmt.Println("No stream version available at http://localhost:1337/nostream")
 	if err := http.ListenAndServe("localhost:1337", nil); err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 	}
